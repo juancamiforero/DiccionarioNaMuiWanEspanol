53ff310fae1e6e3573c69ccbf35acc6c9bfab65f
Fher95 <luisfgomezg@unicauca.edu.co>
2020-03-24 15:17:22 -0500

    Corregido problema del icono de sonido

--- src/app/lista-palabras.ts
+++ src/app/lista-palabras.ts
@@ -1,9 +1,9 @@
 import { Palabra } from './palabra';
 
 export const PALABRAS: Palabra[] = [
-    { id: 1, palabra1: 'perro', palabra2: 'dog', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: ''},
-    { id: 2, palabra1: 'gato', palabra2: 'cat', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: ''},
-    { id: 3, palabra1: 'elefante', palabra2: 'elephant', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: ''},
-    { id: 4, palabra1: 'pez', palabra2: 'fish', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: ''},
-    { id: 5, palabra1: 'buho', palabra2: 'owl', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: ''},
+    { id: 1, palabra1: 'perro', palabra2: 'dog', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: '../assets/imagenes/imagen1.jpg'},
+    { id: 2, palabra1: 'gato', palabra2: 'cat', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: '../assets/imagenes/imagen1.jpg'},
+    { id: 3, palabra1: 'elefante', palabra2: 'elephant', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: '../assets/imagenes/imagen1.jpg'},
+    { id: 4, palabra1: 'pez', palabra2: 'fish', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: '../assets/imagenes/imagen1.jpg'},
+    { id: 5, palabra1: 'buho', palabra2: 'owl', rutaAudio: '../assets/audios/audio1.mp3', rutaImg: '../assets/imagenes/imagen1.jpg'},
 ]
